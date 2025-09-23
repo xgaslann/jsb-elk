bd7674cbfb12d8640fa749deb3a8b940c0bded30
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-23 23:03:06 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/jsbelk/JsbElkApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.jsbelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsbElkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgaslan/jsbelk/JsbElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgaslan.jsbelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsbElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
